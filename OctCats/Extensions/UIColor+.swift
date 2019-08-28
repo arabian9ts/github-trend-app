d43015668252fdb8bc907b6acca3102c59a864d1
arabian9ts <nok14.tm59@gmail.com>
2019-08-28 17:07:20 +0900

    uicolor extension

---
+++ OctCats/Extensions/UIColor+.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor+.swift
+//  OctCats
+//
+//  Created by arabian9ts on 2019/08/28.
+//  Copyright © 2019 arabian9ts. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "invalid Red Value")
+        assert(green >= 0 && green <= 255, "Invalid Green Value")
+        assert(blue >= 0 && blue <= 255, "Invalid Blue Value")
+        
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: 1.0
+        )
+    }
+
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
