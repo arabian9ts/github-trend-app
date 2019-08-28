a8b6087bb03ba76b2910563205b65269192ba619
arabian9ts <nok14.tm59@gmail.com>
2019-08-28 17:03:53 +0900

    lib

---
+++ OctCats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OctCats
+//
+//  Created by arabian9ts on 2019/08/28.
+//  Copyright © 2019 arabian9ts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
